4e9ce718384526b145c7752ceee9606207263aaf
Eduard Caziuc <caziuc.eduard@yahoo.com>
2018-04-26 16:49:50 +0300

    Initial Commit

---
+++ Resume App/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Resume App
+//
+//  Created by Eduard Caziuc on 26/04/2018.
+//  Copyright © 2018 Eduard Caziuc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

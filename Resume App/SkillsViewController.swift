b6c857e6bff3ff85bcb318feb475f292649e0395
Eduard Caziuc <caziuc.eduard@yahoo.com>
2018-04-28 19:07:58 +0300

    Initial Commit

--- Resume App/SkillsViewController.swift
+++ Resume App/SkillsViewController.swift
@@ -1,25 +1,24 @@
 //
-//  SecondViewController.swift
+//  SkillsViewController.swift
 //  Resume App
 //
-//  Created by Eduard Caziuc on 26/04/2018.
+//  Created by Eduard Caziuc on 28/04/2018.
 //  Copyright © 2018 Eduard Caziuc. All rights reserved.
 //
 
 import UIKit
 
-class SecondViewController: UIViewController {
-
+class SkillsViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-

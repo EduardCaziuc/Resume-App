ff941c47cfcbe20fd942fd592d769d79b77a6eb2
Eduard Caziuc <caziuc.eduard@yahoo.com>
2018-04-28 20:05:25 +0300

    First View Controller Done

--- Resume App/AboutViewController.swift
+++ Resume App/AboutViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class AboutViewController: UIViewController {
 
+    @IBOutlet weak var resumePhotoImageView: UIImageView!
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        resumePhotoImageView.layer.cornerRadius = resumePhotoImageView.frame.size.height/2
+        resumePhotoImageView.layer.borderWidth = 3
+       resumePhotoImageView.layer.borderColor = #colorLiteral(red: 0.1298420429, green: 0.1298461258, blue: 0.1298439503, alpha: 1)
+       resumePhotoImageView.clipsToBounds = true
     }
 
     override func didReceiveMemoryWarning() {
